6d179e1e924586bafc7fbefeda0d9d0a35bf47fb
Yaakov Davidovici <ydavidovici@gmail.com>
2024-07-14 15:06:13 -0400

    added file upload controller, cleaned up, added file upload route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileUploadController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/test', function () {
     return 'Test route is working';
 });
+
+Route::get('upload', function () {
+    return view('upload');
+});
+
+Route::post('upload', [FileUploadController::class, 'store'])->name('file.upload');
