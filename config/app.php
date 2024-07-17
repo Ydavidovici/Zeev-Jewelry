60adbbd28e19d683bfc3616eafc0a168992d299e
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2024-07-17 11:53:29 -0400

    added cart and basic checkout

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => env('APP_NAME', 'Laravel'),
-    'env' => env('APP_ENV', 'production'),
+    '.env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
     'url' => env('APP_URL', 'http://localhost'),
     'timezone' => env('APP_TIMEZONE', 'UTC'),
