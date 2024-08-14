bf8e3999d7e1ba781c8ebeef92bb306342955fd9
Yaakov Davidovici <ydavidovici@gmail.com>
2024-08-14 10:19:47 -0400

    refactored for api routes

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer bg-light text-center text-lg-start">
-    <div class="container p-4">
-        <p>&copy; {{ date('Y') }} Zeev Jewelry. All rights reserved.</p>
-    </div>
-</footer>
